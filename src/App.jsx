a247cbbc28a33fb015b5977784f8e270bd575210
MZainalFuad <mzainalfuad99@gmail.com>
2024-05-05 17:12:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import List from "./pages/list/List";
+import Single from "./pages/single/Single";
+import New from "./pages/new/New";
+import { productInputs, userInputs } from "./formsource";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h3>Mohammad Zainal Fuad</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+          <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/users">
+            <Route index element={<List />} />
+            <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
+            <Route path=":userId" element={<Single />} />
+          </Route>
+          <Route path="/products">
+            <Route index element={<List />} />
+            <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
+            <Route path=":productId" element={<Single />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  
+  );
 }
 
-export default App
+export default App;
